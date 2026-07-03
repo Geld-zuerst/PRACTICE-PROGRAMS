77e54dbc803ebecd34519f39abf256bf288b4b62
Harsh Tiwari <harsh.tiwari7112@gmail.com>
2026-07-03 12:46:59 +0530

    Update Classroom.py

--- Classroom.py
+++ Classroom.py
@@ -1,56 +1,92 @@
-class School:
-    def __init__(self, RRHS):
-        self.School = RRHS 
+# ~~~~~~~~~CLASSROOM~~~~~~~~~
 
-    def info(self, Name):
-        self.name = Name
+info = {
+    "Harsh Tiwari": {
+        "Name": "Harsh Tiwari",
+        "Class": "12th",
+        "Academics": "Harsh is an outstanding student who excels both in academics and sports. Harsh is a talented athlete with remarkable skills in cricket and football."
+    },
 
-    def Class(self, Class, Roll_no):
-        self.Class = Class
-        if self.Class > 12 or self.Class <= 0:
-            print("You entered invalid format")
+    "Ankit Dubey": {
+        "Name": "Ankit Dubey",
+        "Class": "12th",
+        "Academics": "Ankit is an outstanding student who excels both in academics and sports. Ankit is a talented athlete with remarkable skills in cricket and volleyball."
+    },
+
+    "Nikhil Tiwari": {
+        "Name": "Nikhil Tiwari",
+        "Class": "12th",
+        "Academics": "Nikhil is an outstanding student who excels both in academics and sports. Nikhil is a talented athlete with remarkable skills in cricket and football."
+    }
+}
+
+# Add info
+# Add info
+def add_info():
+    pass
+
+# Delete info
+def delete_info(name):
+    if name in info:
+        info.pop(name)
+        print("\nStudent deleted successfully!")
+    else:
+        print("\nStudent not found!")
+
+# Main Logic
+
+while True:
+    print("\n========== CLASSROOM ==========")
+    print("1 : See Student Informations")
+    print("2 : Edit Student Informations")
+    print("3 : Exit")
+
+    choice = input("\nWhat do you want to do? ")
+
+    if choice == "1":
+        print("\n===== STUDENT DETAILS =====")
+        for key, value in info.items():
+            print(f"{key}:")
+            print(value)
+            print("-" * 40)
+
+    elif choice == "2":
+        print("\n~~ Editable Options ~~")
+        print("1 : Add New Student Information")
+        print("2 : Delete Student Information")
+
+        edit = input("What do you want to do? ")
+
+        if edit == "1":
+            add_info()
+
+            print("\n===== UPDATED DETAILS =====")
+            for key, value in info.items():
+                print(f"{key}:")
+                print(value)
+                print("-" * 40)
+
+        elif edit == "2":
+            print("\nAvailable Students:")
+            for key in info:
+                print("-", key)
+
+            delete = input("\nEnter full name of student: ")
+            delete_info(delete)
+
+            print("\n===== UPDATED DETAILS =====")
+            for key, value in info.items():
+                print(f"{key}:")
+                print(value)
+                print("-" * 40)
+
         else:
-            print(f"Welcome {self.name} from class {self.Class}th in {self.School}")
+            print("\nInvalid edit option!")
 
-        self.roll_no = Roll_no
-        print("----------------------------------------------------------------------")
+    elif choice == "3":
+        print("\nThank you for using the Classroom Management System.")
+        print("Goodbye! 👋")
+        break
 
-    def more_info(self):
-        options = {"1" : "See Information",
-                   "2" : "See Academics"
-            }
-        self.Harsh = "His father's name is ABC and mother's name is DEF. He is from Rohtas, Bihar."
-        self.Nikhil = "His father's name is MNO and mother's name is PQR. He is from Patna, Bihar."
-        self.Ankit = "His father's name is GHI and mother's name is KLM. He is from Arrah, Bihar."
-
-
-        self.Harsh_info = "He have a Very Good Academics. He scored 95% in 8th, 97% in 9th or 93% in pre boards. Harsh is an outstanding student who excels both in academics and sports. He consistently performs well in all subjects, showing a strong grasp of concepts and a disciplined approach to learning. His curiosity and active participation in class make him a valuable asset to the academic environment. In addition to his academic achievements, Harsh is a talented athlete with remarkable skills in cricket, basketball, and football. He has represented his house in multiple inter-house competitions and brought home several medals and trophies. Harsh exemplifies a perfect balance between academics and extracurricular excellence, making us proud as an institution."
-        self.Nikhil_info = "Nikhil is a sincere and academically strong student who consistently performs well in his studies. He has shown a keen interest in subjects like Mathematics and Science, often going beyond classroom learning to explore concepts deeply. His discipline, punctuality, and dedication make him a role model for his peers. While academics remain his strong suit, Nikhil also participates in sports and physical activities. Though he maintains an average performance in sports, he displays good team spirit and willingness to improve. With the right balance, Nikhil continues to grow as a well-rounded student both in academics and extracurricular areas."
-        self.Ankit_info = "Ankit is a bright and dedicated student who has consistently performed well in his academics. He demonstrates a clear understanding of subjects and approaches his studies with focus and sincerity. His disciplined nature and positive attitude make him a respected member of the classroom. Beyond academics, Ankit stands out as an exceptional cricket player. His performance on the field is commendable, and he has brought great pride to the school with his talent and sportsmanship. Balancing both studies and sports with excellence, Ankit is a fine example of an all-rounder and continues to inspire his peers with his achievements."
-    
-
-        operation = input(f"Enter what you want to see? \n{options} ").strip()
-
-        if operation == "1":
-            if self.name == "Harsh":
-                print(f"\nAbout Harsh: {self.Harsh}")
-            elif self.name == "Nikhil":
-                print(f"\nAbout Nikhil: {self.Nikhil}")
-            elif self.name == "Ankit":
-                print(f"\nAbout Ankit: {self.Ankit}")
-            else:
-                print("\nInvalid information about student")
-        elif operation == "2":
-            if self.name == "Harsh":
-                print(f"\nAcademics profile of Harsh: {self.Harsh_info}")
-            elif self.name == "Nikhil":
-                print(f"\nAcademics profile of Nikhil: {self.Nikhil_info}")
-            elif self.name == "Ankit":
-                print(f"\nAcademics profile of Ankit: {self.Ankit_info}")
-            else:
-                print("\nInvalid information about student")
-
-S1 = School("RRHS")
-S1.info((input(str("Enter Student Name: "))))
-S1.Class(int(input("Enter Class: ")), int(input("Enter Roll no: ")))
-S1.more_info()
+    else:
+        print("\nInvalid choice! Please enter 1, 2, or 3.")
